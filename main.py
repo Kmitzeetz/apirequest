141eae26e33300fcb5f5e63aceab6927da31d684
Kmitzeetz <92951205+Kmitzeetz@users.noreply.github.com>
2022-02-11 17:55:09 +0100

    POST

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 import argparse
+from requests.structures import CaseInsensitiveDict
 
 # apirequest v0.1
 # 2022-02-10
@@ -9,9 +10,9 @@ import argparse
 now = datetime.datetime.now()
 tStamp = now.strftime("%m/%d/%Y, %H:%M:%S")
 
-def user(desc = ""):
+def user(desc = "#"):
     if desc:
-        i = input("# USER (", desc, "):")
+        i = input(desc, "\n# USER: ")
     else:
         i = input('# USER: ')
     if i == "":
@@ -25,13 +26,17 @@ def menu():
     u = user()
     if u == "help":
         cmds()
+    elif u == "get":
+        get()
+    elif u == "post":
+        post()
     else:
         menu()
 
 
 def get():
     print(tStamp, "! ", messaging["GT_star"])
-    i = input("! User: ")
+    user()
     resp = requests.get(i)
     print(tStamp, "! ", messaging["GT_rtrn"])
     print(resp)
@@ -42,17 +47,38 @@ def fileloader():
     print(tStamp, "! ", messaging["FF_star"])
     try:
         f = open("body.json")
+        return f
     except FileNotFoundError:
-        print(tStamp, "! ", messaging["FF_noJS"])
-        
+        print()
     try:
         f = open("body.xml")
+        return f
     except FileNotFoundError:
         print(tStamp, "! ", messaging["FF_noEr"])
 
 
+
+def post():
+    url = user("Provide URL")
+    if url == "":
+        url = "https://reqbin.com/sample/post/json"
+    else:
+        enc = user("Body? (no/auto/xml/json)")
+        if enc == "no":
+            headers = CaseInsensitiveDict()
+            headers["Content-Type"] = "application/json"
+            headers["Content-Length"] = "0"
+
+            resp = requests.post(url, headers=headers)
+        else:
+            fileloader()
+
+    print(resp.status_code)
+    menu()
+
+
 def cmds():
-    print("\nversion v0.1, only get call is supported as a PoC")
+    print("\nversion v0.1, only get/post calls are supported as a PoC")
     print("request <url>    - API call with no authentication\n\n")
     menu()
 
@@ -61,13 +87,13 @@ def cmds():
 messaging = {
     "MN_star" : "\napirequest v0.1, hello there!\n ",
     "MN_menu" : "Type 'help' for help",
-    "MN_reqt" : "Choose the type of request (POST/GET/DELETE):",
     "GT_star" : "GET URL: ",
     "GT_rtrn" : "STATUS: ",
+    "MN_reqt" : "Choose the type of request (POST/GET/DELETE):",
     "FF_star" : "Detecting request body",
     "FF_noJS" : "JSON not found",
     "FF_noXM" : "XML not found",
-    "FF_noEr" : "No request body detected. Provide body.json or body.xml file to directory"
+    "FF_noEr" : "No request body detected. Testing empty API call to https://reqbin.com/sample/post/json"
 }
 
 
